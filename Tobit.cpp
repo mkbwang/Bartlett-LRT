de22a105a92cab2b5bca09023cb8dbab6cdff7b2
Mukai Wang <wangmk@umich.edu>
2023-09-23 12:59:34 -0400

    First draft of tobit model estimation functions.

---
+++ Tobit.cpp
@@ -0,0 +1,62 @@
+//
+// Created by wangmk on 21/09/23.
+//
+
+#include "Tobit.h"
+
+
+double tobitllk(unsigned ndim, const double* params, double* grad, void* input){
+
+    auto inputdata= (const tobitinput *) input;
+    vec rho(params, ndim-1); // beta/sigma
+    double omega = params[ndim-1]; // 1/sigma
+
+    vec z = omega*inputdata->Y - inputdata->X * rho;
+    vec cumnorm_z = normpdf(z);
+
+    // calculate loglikelihood
+    vec llk = inputdata->Delta % (-0.5*square(z) -0.5*log(2*datum::pi) + log(omega))  + \
+        (1-inputdata->Delta) % log(cumnorm_z); // log likelihood contributed from each observation
+
+    // calculate gradient
+    vec deriv_rho(grad, ndim-1, false, true);
+    vec deriv_z = -inputdata->Delta % z + \
+        1/sqrt(2*datum::pi) * (1-inputdata->Delta) / cumnorm_z % exp(-0.5*square(z));
+
+    deriv_rho =  - inputdata->X.t() * deriv_z; // gradient for the elements of rho
+    grad[ndim-1] = accu(inputdata->Delta / omega) + accu(deriv_z % inputdata->Y);
+
+    // return log likelihood
+    return accu(llk);
+}
+
+
+
+tobitoutput estimation(void *input, bool null){
+
+    auto inputdata= (const tobitinput *) input;
+    const unsigned int n_dim = inputdata->X.n_cols+1;
+
+    // optimization object
+    nlopt_opt opt = nlopt_create(NLOPT_LD_LBFGS, n_dim);
+    nlopt_set_lower_bound(opt, n_dim-1, 0); // the inverse scale parameter is positive
+    if (null) { // fitting null model
+        nlopt_set_lower_bound(opt, 1, 0);
+        nlopt_set_upper_bound(opt, 1, 0);
+    }
+    nlopt_set_max_objective(opt, tobitllk, input);
+
+    // set up the parameter vector to estimate
+    vec param_estimate(n_dim, fill::zeros);
+    param_estimate(0) = mean(inputdata->Y)/stddev(inputdata->Y); // initialize the intercept
+    param_estimate(n_dim-1) = 1/stddev(inputdata->Y); // initialize the inverse of standard deviation
+    double *param_pt = param_estimate.memptr();
+    double llk; //loglikelihood
+
+    nlopt_result status = nlopt_optimize(opt, param_pt, &llk);
+    nlopt_destroy(opt);
+    tobitoutput output(param_estimate, llk, status);
+
+    return output;
+}
+
---
+++ Tobit.h
@@ -0,0 +1,40 @@
+//
+// Created by wangmk on 21/09/23.
+//
+#include <iostream>
+#include <cmath>
+#include <nlopt.h>
+#include <armadillo>
+
+using namespace std;
+using namespace arma;
+
+#ifndef BARTLETT_LRT_TOBIT_H
+#define BARTLETT_LRT_TOBIT_H
+
+//TODO: constructor
+struct tobitinput{
+    vec Y; // log count ratio
+    vec Delta; // censorship
+    mat X; // covariate matrix
+    tobitinput(vec dependent_vec, vec censor_vec, mat covar_mat):
+        Y(dependent_vec), Delta(censor_vec), X(covar_mat){}
+};
+
+
+//TODO: constructor
+struct tobitoutput{
+    vec params; // estimated parameter
+    double llk; // log likelihood
+    nlopt_result status; // optimization status
+    tobitoutput(vec estimate, double maxllk, nlopt_result outcome):
+        params(estimate), llk(maxllk), status(outcome){}
+};
+
+// tobit loglikelihood function
+double tobitllk(unsigned ndim, const double* params, double* grad, void* input);
+
+// tobit estimation
+tobitoutput estimation(const tobitinput* input, bool null=false);
+
+#endif //BARTLETT_LRT_TOBIT_H
